e03b7da429982d92d61a42c138b08aae2ff86a81
rajutalukdar1 <lalutaluker@gmail.com>
2023-05-01 02:49:05 +0600

    update

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -50,6 +50,7 @@ const makePayment = async (req, res) => {
         const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
 
         sslcz.init(data).then(apiResponse => {
+            console.log(apiResponse.GatewayPageURL);
             data.uid = user.uid
             req.app.set("data", data)
             let GatewayPageURL = apiResponse.GatewayPageURL
