1f2cf3a0ea7497b428e9d016a3e8c7aa030c0aee
kazirofiq <rofiqul01926881485@gmail.com>
2023-04-30 14:33:39 -0700

    profilet update

--- controllers/userController.js
+++ controllers/userController.js
@@ -18,6 +18,18 @@ const updateUser = async (req, res) => {
 
     res.send(result)
 }
+const updateUserByUid = async (req, res) => {
+    const result = await usersCollection().updateOne(
+        {
+            uid: req.query.uid
+        },
+        {
+            $set: req.body
+        }
+    )
+
+    res.send(result)
+}
 
 const deleteUser = async (req, res) => {
     const result = await deleteDoc(req, usersCollection)
@@ -30,6 +42,13 @@ const getAUser = async (req, res) => {
 
     res.send(result || {})
 }
+const getAUserByUid = async (req, res) => {
+    const result = await usersCollection().findOne({
+        uid: req.query.uid
+    })
+
+    res.send(result || {})
+}
 
 module.exports = {
     getAllUsers,
@@ -37,4 +56,6 @@ module.exports = {
     updateUser,
     deleteUser,
     getAUser,
+    getAUserByUid,
+    updateUserByUid,
 }
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,20 +1,20 @@
-const { getAllUsers, saveUser, updateUser, deleteUser, getAUser } = require("../controllers/userController")
+const { getAllUsers, saveUser, updateUser, deleteUser, getAUser, getAUserByUid, updateUserByUid } = require("../controllers/userController")
 
 const userRouter = require("express").Router()
-
+userRouter.get("/uid", getAUserByUid)
+// update a user by uid
+userRouter.patch("/uid", updateUserByUid)
 // get all users
 userRouter.get("/", getAllUsers)
 
 // save a user
 userRouter.post("/", saveUser)
 
-// update a user by uid
-userRouter.patch("/:id", updateUser)
-
 // delete a user by uid
 userRouter.delete("/:id", deleteUser)
 
 // get a user by uid
 userRouter.get("/:id", getAUser)
+
 
 module.exports = userRouter
