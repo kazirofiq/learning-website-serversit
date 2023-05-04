d4e975f5890940c0d603663bb6a767f6a4a750f0
rajutalukdar1 <lalutaluker@gmail.com>
2023-05-05 01:40:34 +0600

    vdo

--- controllers/modulesController.js
+++ controllers/modulesController.js
@@ -1,5 +1,11 @@
-const { contentsCollection } = require("../mongoDBConfig/collections")
+const { contentsCollection, modulesCollection } = require("../mongoDBConfig/collections")
 const { createDoc } = require("../utils/mongoQueries")
+
+const saveModules = async (req, res) => {
+    const result = await modulesCollection().insertMany(req.body)
+
+    res.send(result)
+}
 
 const saveContent = async (req, res) => {
     const result = await createDoc(req, contentsCollection)
@@ -7,6 +13,17 @@ const saveContent = async (req, res) => {
     res.send(result)
 }
 
+const saveQuiz = async (req, res) => {
+    const result = await contentsCollection().insertMany(req.body)
+    // console.log(result.insertedIds.map((id) => ({ number: id, moduleNo: req.body[i], routeName: "quiz" })));
+    console.log(result);
+    const data = Object.keys(result.insertedIds).map((key, i) => ({ number: result.insertedIds[key], moduleNo: req.body[i].moduleNo, routeName: "quiz" }))
+
+    res.send(data)
+}
+
 module.exports = {
     saveContent,
+    saveQuiz,
+    saveModules,
 }
--- routes/modulesRouter.js
+++ routes/modulesRouter.js
@@ -1,9 +1,15 @@
-const { saveContent } = require("../controllers/modulesController")
+const { saveContent, saveQuiz, saveModules } = require("../controllers/modulesController")
 
 const modulesRouter = require("express").Router()
 
+// save modules quiz
+modulesRouter.post("/contents/quiz", saveQuiz)
+
 // save modules content
 modulesRouter.post("/contents", saveContent)
+
+// save all modules
+modulesRouter.post("/", saveModules)
 
 
 module.exports = modulesRouter
