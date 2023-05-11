e1beb218b7b7f2469c9d63333619b596a195845a
rajutalukdar1 <lalutaluker@gmail.com>
2023-05-12 00:55:54 +0600

    rechore

--- controllers/downloadLimitController.js
+++ controllers/downloadLimitController.js
@@ -1,4 +1,5 @@
 
+const { ObjectId } = require("mongodb")
 const { resoursesDownLimitCollection } = require("../mongoDBConfig/collections")
 const { readDoc, createDoc, updateDoc, deleteDoc, readOneDoc } = require("../utils/mongoQueries")
 
@@ -14,16 +15,18 @@ const postResourceDownLimit = async (req, res) => {
     res.send(result)
 }
 const getADownLimitUserByUid = async (req, res) => {
-    const result = await resoursesDownLimitCollection().findOne({
-        userEmail: req.query.userEmail
-    })
+    const downloadDate = req.query.downloadDate
+    const userEmail = req.query.userEmail
+    const result = await resoursesDownLimitCollection().findOne({ downloadDate: downloadDate, userEmail: userEmail })
 
     res.send(result || {})
 }
-const updateUserByUidDownLimit = async (req, res) => {
+const updateUserByIdDownLimit = async (req, res) => {
+    const id = req.params.id
+
     const result = await resoursesDownLimitCollection().updateOne(
         {
-            UserUid: req.query.UserUid
+            _id: new ObjectId(id)
         },
         {
             $set: req.body
@@ -33,10 +36,38 @@ const updateUserByUidDownLimit = async (req, res) => {
     res.send(result)
 }
 
+const getDownLimit = async (req, res) => {
+    const reviews = await resoursesDownLimitCollection().find({
+        email: req.params.email
+    }).toArray();
+    res.send(reviews)
+}
+
+const saveDownlimit = async (req, res) => {
+    const findQuery = await resoursesDownLimitCollection().findOne({ [Object.keys(req.body)[0]]: { "$exists": true } });
+
+    if (findQuery) {
+        const result = await resoursesDownLimitCollection().updateOne(
+            {
+                _id: findQuery._id
+            },
+            {
+                $push: { [Object.keys(req.body)[0]]: Object.values(req.body)[0][0] }
+            }
+        )
+        res.send(result);
+        return
+    }
+
+    const result = await createDoc(req, resoursesDownLimitCollection);
+    res.send(result)
+}
 module.exports = {
     postResourceDownLimit,
     getResourceDownLimit,
     getADownLimitUserByUid,
-    updateUserByUidDownLimit
-    
+    updateUserByIdDownLimit,
+    getDownLimit,
+    saveDownlimit
+
 }
